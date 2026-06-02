56f65ee2fa01655e144db19bd4e454bc9fb3cd2e
lilynas <lilynas@users.noreply.github.com>
2026-06-02 10:34:00 +0800

    Prevent provider reset on data changes

--- app/src/main/java/com/phrasevoice/data/repository/SettingsRepository.kt
+++ app/src/main/java/com/phrasevoice/data/repository/SettingsRepository.kt
@@ -8,6 +8,7 @@ import com.phrasevoice.data.local.PhraseVoicePreferenceKeys
 import com.phrasevoice.data.local.safeData
 import com.phrasevoice.data.model.UserSettings
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
@@ -21,6 +22,7 @@ class SettingsRepository(
                 UserSettings(),
             )
         }
+        .distinctUntilChanged()
 
     suspend fun updateSettings(transform: (UserSettings) -> UserSettings) {
         dataStore.edit { preferences ->
