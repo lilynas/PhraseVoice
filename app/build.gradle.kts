64f10bd35f35eca49f57fead5ddb8578eebf47ce
lilynas <lilynas@users.noreply.github.com>
2026-06-14 16:32:05 +0800

    Release v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.shirone.phrasevoice"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
